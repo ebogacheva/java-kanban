29ebc0637536aedc8bb9679c2e7575d4c707da97
Ekaterina Bogacheva <bogacheva.e@gmail.com>
2022-12-12 20:16:31 +0300

    fix: поправила equals and hashcode у Task - только id

--- src/ru/yandex/practicum/ebogacheva/sprint3/Task.java
+++ src/ru/yandex/practicum/ebogacheva/sprint3/Task.java
@@ -38,15 +38,12 @@ public class Task {
         if (this == o) return true;
         if (!(o instanceof Task)) return false;
         Task task=(Task) o;
-        return Objects.equals(id, task.id)
-                && Objects.equals(title, task.title)
-                && Objects.equals(description, task.description)
-                && status == task.status;
+        return Objects.equals(id, task.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title, description, status);
+        return Objects.hash(id);
     }
 
     @Override
