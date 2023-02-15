5d636d0286f427d2fa705bce8209a8bd1fa946bc
Ekaterina Bogacheva <bogacheva.e@gmail.com>
2023-02-15 14:17:11 +0300

    feat: добавила KVTaskCleint класс

---
+++ src/main/java/ru/yandex/practicum/ebogacheva/tracker/server/KVTaskClient.java
@@ -0,0 +1,69 @@
+package ru.yandex.practicum.ebogacheva.tracker.server;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+
+public class KVTaskClient {
+
+    private String API_TOKEN;
+    private final String serverURL;
+    private final HttpClient httpClient;
+
+    public KVTaskClient(String serverURL) throws IOException, InterruptedException {
+        this.serverURL = serverURL;
+        this.httpClient = HttpClient.newHttpClient();
+        register();
+    }
+
+    private void register() throws IOException, InterruptedException {
+        URI uri = URI.create(this.serverURL + "/register");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(uri)
+                .GET()
+                .header("Content-Type", "application/json")
+                .version(HttpClient.Version.HTTP_1_1)
+                .build();
+        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+        HttpResponse<String> response = httpClient.send(request, handler);
+        if (response.statusCode() != ResponseCode.OK_200.getCode()) {
+            System.out.println("Не удалось зарегистрироваться и получить код доступа, ошибка :" + response.statusCode());
+        }
+        this.API_TOKEN = response.body();
+    }
+
+    public void put(String key, String json) throws IOException, InterruptedException {
+        URI uri = URI.create(this.serverURL + "/save/" + key + "?API_TOKEN=" + this.API_TOKEN);
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(uri)
+                .POST(HttpRequest.BodyPublishers.ofString(json))
+                .header("Content-Type", "application/json")
+                .version(HttpClient.Version.HTTP_1_1)
+                .build();
+        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8);
+        HttpResponse<String> response = httpClient.send(httpRequest, handler);
+        if (response.statusCode() != ResponseCode.OK_200.getCode()) {
+            System.out.println("Не удалось сохранить данные, ошибка :" + response.statusCode());
+        }
+    }
+
+    public String load(String key) throws IOException, InterruptedException {
+        URI uri = URI.create(this.serverURL + "/load/" + key + "?API_TOKEN=" + this.API_TOKEN);
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(uri)
+                .header("Content-Type", "application/json")
+                .version(HttpClient.Version.HTTP_1_1)
+                .build();
+        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+        HttpResponse<String> response = httpClient.send(request, handler);
+        if (response.statusCode() != ResponseCode.OK_200.getCode()) {
+            System.out.println("Не удалось получить данные, оОшибка :" + response.statusCode());
+        }
+        return response.body();
+    }
+
+}
